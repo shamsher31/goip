063fa3cdffbf4be83c4855043f6bb30c0406854a
Shamsher Ansari <ansarisam47@gmail.com>
2023-04-13 11:38:23 +0530

    Add missing test cases

--- ip_test.go
+++ ip_test.go
@@ -1,11 +1,41 @@
 package ip
 
-import "testing"
+import (
+	"net/http"
+	"testing"
+)
 
 func TestLocal(t *testing.T) {
-
+	localIP, err := Local()
+	if err != nil {
+		t.Errorf("Failed to get local IP address: %v", err)
+	}
+	if localIP == nil {
+		t.Error("Local IP address is nil")
+	}
 }
 
 func TestRemote(t *testing.T) {
+	// Create a mock HTTP request
+	req, err := http.NewRequest("GET", "http://example.com", nil)
+	if err != nil {
+		t.Errorf("Failed to create HTTP request: %v", err)
+	}
+	req.RemoteAddr = "192.168.1.1:1234"
 
+	remoteIP, err := Remote(req)
+	if err != nil {
+		t.Errorf("Failed to get remote IP address: %v", err)
+	}
+	if remoteIP.String() != "192.168.1.1" {
+		t.Errorf("Expected remote IP address to be 192.168.1.1, got %v", remoteIP)
+	}
+
+	// Test with invalid IP address
+	req.RemoteAddr = "invalid-ip"
+	_, err = Remote(req)
+	if err == nil {
+		t.Error("Expected error for invalid IP address")
+	}
 }
+
